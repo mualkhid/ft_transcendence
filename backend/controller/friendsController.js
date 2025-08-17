a70b12201aa57ea8236cad2b302657cba54a5928
Maha Faisal <mafaisal@lab2r1s12.42abudhabi.ae>
2025-08-17 15:14:55 +0400

    auth functions connect reads and writes to db

---
+++ backend/controller/friendsController.js
@@ -0,0 +1,32 @@
+import {prisma} from '../server.js';
+
+// const users = await prisma.user.findMany() gets all the users
+
+
+export async function getFriends(req, reply) {
+
+}
+
+export async function sendRequest(req, reply) {
+
+}
+
+export async function acceptRequest(req, reply) {
+
+}
+
+export async function declineRequest(req, reply) {
+
+}
+
+export async function removeFriend(req, reply) {
+
+}
+
+export async function blockFriend(req, reply) {
+
+}
+
+export async function searchUser(req, reply) {
+
+}
