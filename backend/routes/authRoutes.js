6989a7ab54751bbbb204f897b5992e782ec7deb3
Maha Faisal <mafaisal@lab1r1s6.42abudhabi.ae>
2025-08-25 13:42:16 +0400

    added /api prefix to routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,9 +11,9 @@ import {
 } from '../schema/authSchema.js';
 
 export default function authRoutes(fastify, _opts, done) {
-  fastify.post('/auth/registerUser', registerUserOpts, registerUser);
-  fastify.post('/auth/login', loginOpts, login);
-  fastify.post('/auth/logout', logoutOpts,  logout);
+  fastify.post('/api/auth/registerUser', registerUserOpts, registerUser);
+  fastify.post('/api/auth/login', loginOpts, login);
+  fastify.post('/api/auth/logout', logoutOpts,  logout);
   done();
 }
 
