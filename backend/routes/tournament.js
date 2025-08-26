b3366841ccd6c7ba1108eaf1fa797ea26b7eab92
Sumyah Helal <suhelal@lab1r1s6.42abudhabi.ae>
2025-08-26 14:52:54 +0400

    added

--- backend/routes/tournament.js
+++ backend/routes/tournament.js
@@ -1,19 +1,29 @@
-import { createTournament, getTournamentRequest, joinTournament, nextMatch, resetTournamentRequest } from '../controller/tournamentController.js';
-import { createTournamentSchema, getTournamentSchema, joinTournamentSchema, nextMatchSchema, resetTournamentSchema  } from '../schema/tournamentSchema.js';
+import {
+  createTournament,
+  getCurrentMatchRequest,
+  completeMatchRequest,
+  getTournamentBracketRequest,
+  resetTournamentRequest
+} from '../controller/tournamentController.js';
 
+import {
+  createTournamentSchema,
+  getCurrentMatchSchema,
+  completeMatchSchema,
+  getBracketSchema
+} from '../schema/tournamentSchema.js';
 
 async function tournamentRoutes(fastify, options)
 {
-  fastify.post('/tournament/create', {schema: createTournamentSchema}, createTournament);
+  fastify.post('/tournament/create', { schema: createTournamentSchema }, createTournament);
 
-  fastify.get('/tournament', {schema: getTournamentSchema}, getTournamentRequest);
+  fastify.get('/tournament/current-match', { schema: getCurrentMatchSchema }, getCurrentMatchRequest);
 
-  fastify.post('/tournament/join', {schema: joinTournamentSchema}, joinTournament);
+  fastify.post('/tournament/complete-match', { schema: completeMatchSchema }, completeMatchRequest);
 
-  fastify.get('/tournament/next-match', {schema : nextMatchSchema}, nextMatch);
-
-  fastify.post('/tournament/reset', {schema: resetTournamentSchema}, resetTournamentRequest);
-
+  fastify.get('/tournament/bracket', { schema: getBracketSchema }, getTournamentBracketRequest);
+  
+  fastify.post('/tournament/reset', resetTournamentRequest);
 }
   
 export default tournamentRoutes;
