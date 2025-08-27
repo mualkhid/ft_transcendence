05c403ca79aa3c16fa1f6b80e9f6adaf811a6d44
Maha Faisal <mafaisal@lab1r1s7.42abudhabi.ae>
2025-08-27 16:01:40 +0400

    added authentication hander

--- backend/routes/remoteGameRoutes.js
+++ backend/routes/remoteGameRoutes.js
@@ -1,8 +1,11 @@
 import { handleRemoteGame } from '../controller/remoteGameController.js';
 
+import { authenticate } from '../services/jwtService.js';
+import { trackUserActivity } from '../services/lastSeenService.js';
+
 async function remoteGameRoutes(fastify, options)
 {
-    fastify.get('/remote-game/:matchId', {websocket : true}, (socket, request) => {
+    fastify.get('/remote-game/:matchId', {preHandler: [authenticate, trackUserActivity]}, {websocket : true}, (socket, request) => {
         const { matchId } = request.params;
         handleRemoteGame(socket, matchId);
     });
