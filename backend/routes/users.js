e8577177137257f47a0066b4fc8764690eaf50fc
Sarah Abdalla <sa.abdallah98@gmail.com>
2025-09-04 18:32:12 +0400

    clr branch

--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-import { registerUser, deleteAccount, anonymizeAccount } from '../controller/userController.js';
-import { registerSchema, deleteAccountSchema, anonymizeAccountSchema } from '../schema/userSchema.js';
-import { authenticate } from '../services/jwtService.js';
-
-export default function userRoutes(fastify, options) {
-    // Removed conflicting /register route - using /auth/registerUser instead
-    fastify.delete('/user/delete', { preHandler: authenticate, schema: deleteAccountSchema }, deleteAccount);
-    fastify.post('/user/anonymize', { preHandler: authenticate, schema: anonymizeAccountSchema }, anonymizeAccount);
-}
